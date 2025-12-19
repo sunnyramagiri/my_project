50f3497f96cd4f4bc5d6ea917460e2678ae16eba
sunnyramagiri <136982339+sunnyramagiri@users.noreply.github.com>
2025-12-19 22:28:55 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,32 @@
-resource "aws_instance" "nok" {
-  ami           = "ami-068c0051b15cdb816"
+resource "aws_instance" "nopk" {
+  ami = "ami-0ecb62995f68bb549"
   instance_type = "t3.medium"
-
-  key_name = aws_key_pair.loks.key_name
+  key_name = aws_key_pair.lock.key_name
 
   tags = {
-    Name = "nok"
+    name = "nopk"
+  }
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all_sg"
+  description = "Allow all inbound and outbound traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow-all-sg"
   }
 }
